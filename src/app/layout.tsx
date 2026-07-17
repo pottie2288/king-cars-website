6a76203caeb28ad8608fa5dd5a3c75a879ab8843
pottie2288 <pottie2288@gmail.com>
2026-07-17 03:30:18 +0200

    feat(analytics): add Meta Pixel for Facebook/Instagram tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -131,7 +131,28 @@ export default function RootLayout({
           gtag('config', '${GA_ID}');
         `}
       </Script>
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1987670918567269');
+          fbq('track', 'PageView');
+        `}
+      </Script>
       <body>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1987670918567269&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <FavouritesProvider>
           <div className="min-h-screen bg-white overflow-x-hidden relative">
             <ScrollToTop />
